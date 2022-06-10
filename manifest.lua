4d3212aeb8ac54b7eaf5a570cecadac08831152e
brickedware <106000036+brickedware@users.noreply.github.com>
2022-06-10 19:57:05 -0400

    Manifest file
    Due to the nature of BPCore Engine, you cannot put assets into folders, so organization may be somewhat of a problem.

---
+++ manifest.lua
@@ -0,0 +1,26 @@
+local app = {
+   name = "AnimalCrossingAdvance",
+
+   tilesets = {
+      -- no tilesets yet, the only required tilesets are terrain for all seasons
+   },
+
+   spritesheets = {
+      -- all player, villager, and special spritesheets would be listed here
+   },
+
+   audio = {
+      -- Title may be slightly modified from ACPG
+      -- All tracks may be copied from ACPG in a ACWW/ACCF way, not good at music so original tracks are out of the question
+   },
+
+   scripts = {
+      -- All scripts and modules will go here
+   },
+
+   misc = {
+      -- Language localization files will go here, English will be main and then translation is a possibility
+   }
+}
+
+return app
