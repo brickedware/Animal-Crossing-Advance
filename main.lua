19df13166a24e2f1267ba50142417302bec6d963
brickedware <106000036+brickedware@users.noreply.github.com>
2022-06-11 10:34:59 -0400

    Updated for Animalese
    Bebebese and silence coming soon as they should not be that hard to make

--- main.lua
+++ main.lua
@@ -1,7 +1,15 @@
 fade(1, 0x000000)
 
+local function wait(microseconds) -- SIMPLE WAIT FUNCTION
+    local start = delta()
+    repeat until start >= delta()
+end
+
 local function loadTitle(musId) -- TITLESCREEN FUNCTION
+    sound("m-nintdo.raw")
+    --wait(5000000)
     music(musId,0)
+    -- not done, need a "preview" of the game
 end
 
 local function clearDisplay() -- REFRESH DISPLAY 
@@ -10,7 +18,36 @@ local function clearDisplay() -- REFRESH DISPLAY
         display()
     end
 end
-fade(0)
 
-loadTitle("m-title.raw")
+local function loadTrain(skip)
+    -- nothing yet
+end
+
+local function speak(text,voice)
+    if voice == nil then
+        voice = 0
+    end
+
+    local t = {}
+    local ltext = string.lower(text)
+    ltext:gsub(".",function(c) table.insert(t,c) end)
+
+    for i,v in ipairs(t) do
+        local deltime = 0
+        delta()
+        repeat
+            deltime = deltime + delta()
+            -- nothing
+        until deltime >= 60000
+        if v ~= " " or "," or "!" or "?" then
+            sound("v" .. tostring(voice) .. "-" .. v .. ".raw")
+        end
+    end
+end
+fade(0, 0x000000)
+
+--loadTitle("m-title.raw")
+
+speak("This is where you input the text. ABCDEFGHIJKLMNOPQRSTUVWXZY1234567890.",0)
+
 clearDisplay()
