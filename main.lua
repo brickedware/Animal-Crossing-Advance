ae29b1afbade167ad2dedfce289aea7209ba7d83
brickedware <106000036+brickedware@users.noreply.github.com>
2022-06-13 08:03:28 -0400

    Unfinished speech testing
    Need the text to wrap without any words also wrapping

--- main.lua
+++ main.lua
@@ -1,14 +1,41 @@
-fade(1, 0x000000)
+local function wait(seconds) -- SIMPLE WAIT FUNCTION
+    local us = seconds * 1000000
+    local start = delta()
+    repeat start = start + delta() until start >= us
+end
 
---local function wait(microseconds) -- SIMPLE WAIT FUNCTION <<disabled, does not work yet
---    local start = delta()
---    repeat until start >= delta()
---end
+local function printText(text,x,y,rmargin)
+    txtr(0,"font-8x8.bmp")
+    if rmargin == nil then
+        rmargin = x
+    end
+    local tabl = {}
+    local xx = x
+    local yy = y
+    text:gsub(".",function(c) table.insert(tabl,c) end)
+    for u,v in pairs(tabl) do
+        for i=32,256 do
+            if v == utf8.char(i) then
+                tile(0, xx, yy, i-32)
+            end
+        end
+        if rmargin == false or xx <= 28 - rmargin then
+            xx = xx + 1
+        else
+            yy = yy+1
+            xx = x
+        end
+    end
+end
 
-local function loadTitle(musId) -- TITLESCREEN FUNCTION
+local function loadTitle() -- TITLESCREEN FUNCTION
+    
     sound("m-nintdo.raw")
-    --wait(5000000)
-    music(musId,0)
+    wait(7)
+    
+    --music("m-title.raw",0)
+    --wait(1)
+
     -- not done, need a "preview" of the game
 end
 
@@ -23,31 +50,61 @@ local function loadTrain(skip)
     -- nothing yet
 end
 
-local function speak(text,voice)
+
+local function speech(text,voice,open,color,name)
     if voice == nil then
         voice = 0
+    end 
+
+    printText(name,3,9,false)
+
+    txtr(1,"".. color .."speak.bmp")
+    local t = 0
+    for yy = 0, 13 do
+       for xx = 0, 29 do
+          tile(1, 0 + xx, 7 + yy, t)
+          t = t + 1
+       end
     end
-
-    local t = {}
-    local ltext = string.lower(text)
-    ltext:gsub(".",function(c) table.insert(t,c) end)
-
-    for i,v in ipairs(t) do
+    
+    
+    txtr(0,"font-8x8.bmp")
+    
+    
+    local tabl = {}
+    local xx = 3
+    local yy = 11
+    text:gsub(".",function(c) table.insert(tabl,c) end)
+    for u,v in pairs(tabl) do
+        for i=32,256 do
+            if v == utf8.char(i) then
+                tile(0, xx, yy, i-32)
+            end
+        end
+        if xx <= 25 then
+            xx = xx + 1
+        else
+            yy = yy+1
+            xx = 3
+        end
+        if v ~= " " or "," or "!" or "?" or "" then
+            sound("v" .. tostring(voice) .. "-" .. string.lower(v) .. ".raw")
+        end
         local deltime = 0
         delta()
-        repeat
-            deltime = deltime + delta()
-            -- nothing
-        until deltime >= 60000
-        if v ~= " " or "," or "!" or "?" then
-            sound("v" .. tostring(voice) .. "-" .. v .. ".raw")
-        end
+        if btn(1) == true or btnp(1) or tabl[u+1] ~= utf8.char(32) then
+            repeat
+               deltime = deltime + delta()
+               clear()
+               display()
+           until deltime >= 250
+       end
     end
 end
-fade(0, 0x000000)
+---------------------------------------------------------------------------------------------------
+--BEGIN GAME---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 
---loadTitle("m-title.raw")
-
-speak("This is where you input the text. ABCDEFGHIJKLMNOPQRSTUVWXZY1234567890.",0)
-
+loadTitle()
+speech("So, you've decided to move out? Get your own place? See the world? That's groovy.",0,true,"g","K.K.")
 clearDisplay()
